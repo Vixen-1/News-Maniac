0a202c4fe8f8b2438cc6c3c83c0d2b3e139aaa36
Vixen-1 <ayushisaxena24111999@gmail.com>
2023-10-05 16:29:50 +0530

    second commit

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        this is a news Item
+      <div className="my-3">
+        <div className="card" style={{width: '18rem'}}>
+            <img src={imageUrl?imageUrl:"https://images.indianexpress.com/2023/10/ASIAN-GAMES-LIVEBLOG-DAY-11-NEERAJ-CHOPRA.jpg"} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More...</a>
+            </div>
+        </div>
       </div>
     )
   }
